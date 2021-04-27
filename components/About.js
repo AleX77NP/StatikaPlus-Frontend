5d3ac5f7940ce03521a157ecfa38abe01210d3e7
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-04-27 16:27:09 +0200

    box shadow

--- components/About.js
+++ components/About.js
@@ -13,19 +13,19 @@ const About = () => {
             </Text>
             <Flex flexWrap="wrap" alignItems="center" maxW="900px" justifyContent="center" mx="auto" mt="10">
 
-                <Box as="a" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
+                <Box boxShadow="xl" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
                     <Text fontSize="4xl" mb="2" fontFamily="normal">Neosporiv kvalitet &#x2606;</Text>
                     <Text fontSize="lg" color="gray.500">Preko 30 godina rada. Uvek pratimo moderne trendove i tehnologije.</Text>
                 </Box>
-                <Box as="a" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
+                <Box boxShadow="xl" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
                     <Text fontSize="4xl" mb="2" fontFamily="normal" mb="2">Efikasnost &#x26A1;</Text>
                     <Text fontSize="lg" color="gray.500">Vreme je novac. A novac je prioritet i za Vas, i za nas.</Text>
                 </Box>
-                <Box as="a" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
+                <Box boxShadow="xl" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
                     <Text fontSize="4xl" mb="2" fontFamily="normal" mb="2">Povoljne cene &euro;</Text>
                     <Text fontSize="lg" color="gray.500">Najpovoljnije cene po kvadratu u regionu. Tu smo za Vas.</Text>
                 </Box>
-                <Box as="a" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
+                <Box boxShadow="xl" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
                     <Text fontSize="4xl" mb="2" fontFamily="normal" mb="2">Pouzdanost &#x263A;</Text>
                     <Text fontSize="lg" color="gray.500">Hiljade zadovoljnih klijenata. Hiljade objekata.</Text>
                 </Box>
